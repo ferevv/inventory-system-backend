644f5612850e2dd9a4178599c758a7de54307504
ferevv <felipevillamizar06@hotmail.com>
2024-02-02 18:39:15 +0000

    Delete everything except the backend project

--- Frontend/src/utils/formatDate.js
+++
@@ -1,7 +0,0 @@
-const formatDate = (dateString) => {
-    const options = { day: '2-digit', month: 'short', year: 'numeric' };
-    const formattedDate = new Date(dateString).toLocaleDateString(undefined, options);
-    return formattedDate;
-};
-
-export default formatDate;
--- Frontend/src/utils/trimFormValues.js
+++
@@ -1,7 +0,0 @@
-const trimFormValues = (formData) => {
-    return Object.fromEntries(
-        Object.entries(formData).map(([key, value]) => [key, typeof value === 'string' ? value.trim() : value])
-    );
-}
-
-export default trimFormValues;
